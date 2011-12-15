267093c25fb04677993449d70e6452872a46937e
Lluís Gómez Hernando <lluisgh28@gmail.com>
2011-12-15 10:24:22 +0100

    open de la BD a contactdatamanager

--- src/com/meetme/contacts/ContactsDataManager.java
+++ src/com/meetme/contacts/ContactsDataManager.java
@@ -35,6 +35,7 @@ public class ContactsDataManager {
 	
 	public ArrayList<User> getContacts(){
 		mda  = new MeetMeDbAdapter(mContext);
+		mda.open();
         PreferencesAdapter pa = new PreferencesAdapter(mContext);
         String username = pa.getActiveUsername();
         ArrayList<User> contacts = mda.fetchContacts(username);
