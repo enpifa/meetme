e95f9f9767d9fc49a3f60c5b296376f360220f0d
Lluís Gómez Hernando <lluisgh28@gmail.com>
2011-12-15 08:33:46 +0100

    Logout i change password afegits als settings

--- src/com/meetme/settings/SettingsManager.java
+++ src/com/meetme/settings/SettingsManager.java
@@ -1,5 +1,27 @@
 package com.meetme.settings;
 
-public class SettingsManager {
+import com.meetme.app.MeetMeDbAdapter;
+import com.meetme.app.PreferencesAdapter;
 
+public class SettingsManager {
+	private MeetMeDbAdapter mDbHelper;
+	private PreferencesAdapter pa;
+	public void deleteUser(String username) {
+		mDbHelper.deleteProfile(username);
+		mDbHelper.deleteContactsOfUser(username);
+		mDbHelper.deletePhonesOfUser(username);
+		mDbHelper.deleteWebsOfUser(username);
+		mDbHelper.deleteMailsOfUSer(username);
+		mDbHelper.deleteUser(username);	
+	}
+	
+	public void logout() {
+		pa.setActiveUser(null);
+		
+	}
+	
+	public void changePassword(String password) {
+		mDbHelper.updateUser(pa.getActiveUsername(), password);
+	}
+	
 }
